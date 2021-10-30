13ac759c4667af66440db093ccbe8bb8340aa49d
NedKJaims <47610687+NedKJaims@users.noreply.github.com>
2021-10-29 19:54:22 -0500

    Remove TextRec and Factor camera class

--- Utils/Render/Camera.cs
+++ Utils/Render/Camera.cs
@@ -2,28 +2,24 @@
 
 namespace NQuad.Utils.Render {
     public class Camera {
-        public Matrix Matrix => Matrix.CreateRotationZ(rotation) * Matrix.CreateScale(Zoom) * Matrix.CreateTranslation(target.X - offset.X, target.Y - offset.Y, 0);
-        public Vector2 offset;
-        public Vector2 target;
-        public float rotation { get; set; }
+        public Matrix Matrix => Matrix.CreateRotationZ(Rotation) * Matrix.CreateScale(Zoom) * Matrix.CreateTranslation(Target.X - Offset.X, Target.Y - Offset.Y, 0);
+        
+        public Vector2 Offset;
+        public Vector2 Target;
+        
+        public float Rotation { get; set; }
+
         private float zoom;
         public float Zoom {
             get { return zoom; }
-            set { SetZoom(value); }
+            set { zoom = MathHelper.Clamp(value, -10f, 10f); }
         }
+
         public Camera(Vector2 off, Vector2 targ, float rot = 0f, float _zoom = 1f) {
-            offset = off;
-            target = targ;
-            rotation = rot;
-            zoom = (_zoom > 0f) ? _zoom : 0;
-        }
-        private void SetZoom(float zoom) {
-            if (zoom > -10f && zoom < 10f)
-                this.zoom = zoom;
-        }
-        public void SetRelativeZoom(float zoom) {
-            if (zoom > -10f && zoom < 10f)
-                this.zoom += zoom;
+            Offset = off;
+            Target = targ;
+            Rotation = rot;
+            zoom = MathHelper.Clamp(_zoom, -10f, 10f);
         }
 
     }
